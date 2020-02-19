033a0762286169bcb5b630423e434816b2ab6642
levabala <levabala@ya.ru>
2020-02-19 09:05:14 +0300

    initial

---
+++ commit.sh
@@ -0,0 +1,5 @@
+branch=${2:-master}
+
+git add ./
+git commit -m "$1"
+git push origin $branch
---
+++ src/TestComponent/TestComponent.tsx
@@ -0,0 +1,9 @@
+import './TestComponent.scss';
+
+import React from 'react';
+
+const TestComponent = () => {
+  return <div styleName="b">asd</div>;
+};
+
+export default TestComponent;
